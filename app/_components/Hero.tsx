980a503b3b636da309d7cd357c349f0ce92c1700
PriyanshGupta2002 <guptapriyansh905@gmail.com>
2025-11-29 15:35:08 +0530

    landing page completed

--- app/_components/Hero.tsx
+++ app/_components/Hero.tsx
@@ -1,5 +1,36 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <div className="h-screen w-full overflow-hidden relative">
+      <Image
+        alt="hero"
+        className="object-cover w-full h-full"
+        fill
+        src={"/hero.gif"}
+      />
+
+      <div className="absolute font-pixelify-sans w-full h-full flex flex-col items-center mt-24">
+        <h2 className="font-bold  text-7xl">Start Your</h2>
+        <h2
+          className="font-bold text-8xl  text-yellow-400"
+          style={{
+            textShadow:
+              "2px 2px 0 #000, -2px -2px 0 #000, 2px -2px 0 #000,-2px 2px 0 #000",
+          }}
+        >
+          Coding Adventure
+        </h2>
+        <h2 className="text-3xl mt-5">
+          Beginner Friendly Coding Courses and Projects
+        </h2>
+        <Button className="text-3xl p-6 mt-7" variant={"pixel"}>
+          GET STARTED
+        </Button>
+      </div>
+    </div>
+  );
 };
 
 export default Hero;
